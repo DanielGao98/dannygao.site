db9bd2b888d643585a754a67c5ed5edbe8baa16c
DanielGao98 <dannygao0818@gmail.com>
2020-08-15 23:48:26 +0000

    kkkkkkkkk

---
+++ dannygao.site/public_html/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+
+<?php
+Echo "Cache-Control: no-cache<br>";
+Echo "Content-tyle: text/html <br><br>";
+Echo "<html><br>";
+Echo "<head><br>";
+Echo"<title>PHP Hello World</title><br>";
+Echo "</head><br>";
+Echo "</html>";
