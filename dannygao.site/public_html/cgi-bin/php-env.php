3c8c19e163cd79487854e38770e6fe6c6b7bc538
DanielGao98 <dannygao0818@gmail.com>
2020-08-16 20:35:28 +0000

    bulbasaur

--- dannygao.site/public_html/cgi-bin/php-env.php
+++ dannygao.site/public_html/cgi-bin/php-env.php
@@ -2,6 +2,24 @@
 header('Cache-Control: no-cache');
 header('Content-type: text/html');
 
+echo "<!DOCTYPE html>";
+echo "<html><head><title>Environment Variables</title></head><body>";
+echo "<h1 align ='center'>Environment Variables</h1><hr><br>";
+
+echo "<h1>Environment Variables:</h1><br>";
+
 $allVars = getenv();
-foreach ($allVars as &$var) {
-	echo "
+foreach ($allVars as $var => $value) {
+	echo "<b>" . $var . "</b>: " . $value ;
+	echo"<br>";
+}
+
+echo "<h1>Server Variables:</h1><br>";
+
+foreach ($_SERVER as $svar => $svalue) {
+	echo "<b>" . $svar . "</b>: " . $svalue ;
+	echo"<br>";
+}
+
+echo "</body></html>";
+?>
