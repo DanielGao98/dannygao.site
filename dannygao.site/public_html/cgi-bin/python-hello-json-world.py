2988e1d9b753e22932897659d45be20363638f5a
DanielGao98 <dannygao0818@gmail.com>
2020-08-18 04:27:09 +0000

    hello

---
+++ dannygao.site/public_html/cgi-bin/python-hello-json-world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import cgi
+import json
+from datetime import date
+import os
+
+
+today = date.today()
+stoday = today.strftime("%Y-%m-%d")
+addr = str(os.environ['REMOTE_ADDR'])
+
+message = {'Mesage':'Hello World from Python!', 'date':'Todays date is ' + stoday, 'ipAddress':addr} 
+
+print("Cache-Control: no-cache")
+print("Content-type:text/html\r\n")
+
+x = json.dumps(message)
+print(str(x))
+
